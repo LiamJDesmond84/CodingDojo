f2e0598222ca575974416bb240eb5f68235d1dba
Liam Desmond <liamjdesmond84@gmail.com>
2021-11-20 20:17:26 -0600

    Adding first-jsx

---
+++ Assignments/MERN/React/first-jsx/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h3>Thing I need to do:</h3>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb My. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
