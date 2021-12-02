3e3f82a3d0c246d54bc9069942c42e70bab76ed0
Liam Desmond <liamjdesmond84@gmail.com>
2021-12-02 13:17:05 -0600

    adding Authors

---
+++ Assignments/MERN/Full-Stack-MERN/Authors/client/src/components/AuthorForm.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import axios from "axios"
+import { navigate, Link } from "@reach/router";
+
+const AuthorForm = (props) => {
+    // const { hasBeenSubmitted, setHasBeenSubmitted } = props;
+    const [errors, setErrors] = useState({});
+    const [name, setName] = useState("")
+
+
+    const createAuthor = (e) => {
+        e.preventDefault();
+        axios
+            .post("http://localhost:8000/api/authors", {name})
+            .then((response) => {
+                console.log(response);
+                setName("");
+                navigate("/");
+
+                // setHasBeenSubmitted(!hasBeenSubmitted);
+                
+            })
+            
+            .catch((err) => {
+            console.log(err);
+            setErrors(err.response.data.errors)});
+            
+        };
+
+    return (
+        <div>
+            <h1>Favorite Authors</h1>
+            <h4>Add a new Author</h4>
+            <form onSubmit={createAuthor}>
+                <label>Author Name</label>
+                <input type="text" name="name" value={name.name} onChange={(e) => setName(e.target.value)} />
+                {
+                    errors.name?
+                    <span>{errors.name.message}</span>
+                    :null
+                }
+                <input type="submit" placeholder="Submit" />
+            </form>
+            <button onClick={() => navigate("/")}>Cancel</button>
+            <h1><Link to="/">Home</Link></h1>
+        </div>
+    )
+}
+
+export default AuthorForm
