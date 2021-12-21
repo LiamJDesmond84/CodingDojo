5214f75deef4b57dee06659422ee2f31fcfd526b
Liam Desmond <liamjdesmond84@gmail.com>
2021-12-20 18:59:38 -0600

    updating tabs

--- Assignments/MERN/React/tabs/src/components/Tabs.js
+++ Assignments/MERN/React/tabs/src/components/Tabs.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
-import '../App.css';
+
 
 const Tabs = () => {
     const [tabs, setTabs] = useState("one")
-    const [label, setLabel] = useState(["Tab 1", "Tab 2", "Tab 3"])
-    const [stuff, setStuff] = useState(["Tab 1 content is showing here", "Tab 2 content is showing here", "Tab 3 content is showing here"])
+    const label = ["Tab 1", "Tab 2", "Tab 3"]
+    const stuff = ["Tab 1 content is showing here", "Tab 2 content is showing here", "Tab 3 content is showing here"]
 
 
 
