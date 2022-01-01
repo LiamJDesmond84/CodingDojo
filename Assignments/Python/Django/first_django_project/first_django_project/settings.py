d62491acfa11c5b8146bab02fb121c1bd5b0cfff
Liam Desmond <liamjdesmond84@gmail.com>
2022-01-01 13:52:04 -0600

    adding ignore file

--- Assignments/Python/Django/first_django_project/first_django_project/settings.py
+++ Assignments/Python/Django/first_django_project/first_django_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
+from env import PASS
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6wec3u1(-$fllbsen7ht)=atd$r(9tqn@iu5k(!gl!t9xb=^5('
+SECRET_KEY = PASS
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
