a4ae93dd82292581b0d32981a3ca71a6d4ef3186
Liam Desmond <liamjdesmond84@gmail.com>
2022-01-14 17:50:09 -0600

    Adding Java assignment

---
+++ Assignments/Java/OOP/ZooKeeper/src/com/liam/zookeeper/ZooTest.java
@@ -0,0 +1,23 @@
+package com.liam.zookeeper;
+
+public class ZooTest {
+
+	public static void main(String[] args) {
+
+		Gorrilla zoe = new Gorrilla();
+		
+		zoe.throwThings();
+		zoe.throwThings();
+		zoe.throwThings();
+		
+		zoe.eatBananas();
+		zoe.eatBananas();
+		
+		zoe.climb();
+		
+		int a = zoe.displayEnergy();
+		System.out.println(a);
+		
+	}
+
+}
