4237d817810f6d36195562b5bd5ec6152160ac5f
Liam Desmond <liamjdesmond84@gmail.com>
2022-01-15 21:18:19 -0600

    Adding Java assignment

---
+++ Assignments/Java/OOP/Calculator/src/com/liam/calculator/CalculatorTest.java
@@ -0,0 +1,17 @@
+package com.liam.calculator;
+
+public class CalculatorTest {
+
+	public static void main(String[] args) {
+        Calculator c = new Calculator();
+        
+        c.setOperandOne(10.5);
+        c.setOperandTwo(5.2);
+        c.setOperator('+');
+        
+        c.performOperation();
+        c.getResults();
+
+	}
+
+}
