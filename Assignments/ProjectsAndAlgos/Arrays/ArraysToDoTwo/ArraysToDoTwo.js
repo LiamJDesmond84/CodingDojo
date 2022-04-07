e5c2548fa97211a1d04c2b68fb1175c3f67d50b5
Liam Desmond <liamjdesmond84@gmail.com>
2022-04-07 14:01:10 -0500

    Updating

--- Assignments/ProjectsAndAlgos/Arrays/ArraysToDoTwo/ArraysToDoTwo.js
+++ Assignments/ProjectsAndAlgos/Arrays/ArraysToDoTwo/ArraysToDoTwo.js
@@ -21,58 +21,95 @@
 //# Rotate
 
 
+// var myArr = [1, 2, 4, 6, 7, 8];
+
+// function rotateArr(arr, shiftBy) {
+
+//     var actualMovementsNeeded;
+
+
+//     if (shiftBy > 0) {
+//         actualMovementsNeeded = shiftBy % arr.length;
+
+//     } else {
+
+//         actualMovementsNeeded = Math.abs(shiftBy) % arr.length;
+//     }
+    
+//     //* Handle rotations to the right
+//     if (shiftBy > 0) {
+
+//         //* Loop through all the rotations
+//         for (var x = 0; x < actualMovementsNeeded; x++) {
+
+//             //* Temp value for current last index
+//             var temp = arr[arr.length - 1];
+
+//             //* Loop through to moves items to the right by one index
+//             for (var y = arr.length - 2; y >= 0; y--) {
+//                 arr[y+1] = arr[y];
+//             }
+
+//             //* Put temp value at the beginning of the array
+//             arr[0] = temp;
+//         }
+
+//     //* Handle rotations to the left
+//     } else {
+
+//         for (var x = 0; x < actualMovementsNeeded; x++) {
+
+//             //* Temp value for current first index
+//             var temp = arr[0];
+
+//             //* Loop moves items to the left one index
+//             for (var y = 1; y < arr.length; y++) {
+//                 arr[y-1] = arr[y];
+//                 console.log(arr);
+//             }
+
+//             //* Put temp value at end of array
+//             arr[arr.length - 1] = temp;
+//             console.log(arr);
+//         }
+//     }
+// }
+
+// rotateArr(myArr, 2);
+
+// console.log(myArr);
+
+
+//# Filter Range
+
+
 var myArr = [1, 2, 4, 6, 7, 8];
 
-function rotateArr(arr, shiftBy) {
 
-    var actualMovementsNeeded;
+// Second version with only one for loop
+function filterRange(arr, minVal, maxVal) {
 
+    for (var x = 0; x < arr.length; x++) {
 
-    if (shiftBy > 0) {
-        actualMovementsNeeded = shiftBy % arr.length;
+        // If value is NOT from min to max (inclusively)
+        if (arr[x] < minVal || arr[x] > maxVal) {
 
-    } else {
-
-        actualMovementsNeeded = Math.abs(shiftBy) % arr.length;
-    }
-    
-    //* Handle rotations to the right
-    if (shiftBy > 0) {
-
-        //* Loop through all the rotations
-        for (var x = 0; x < actualMovementsNeeded; x++) {
-
-            //* Handle the single rotation
-            var temp = arr[arr.length - 1];
-
-            //* Loop through to moves items to the right by one index
-            for (var y = arr.length - 2; y >= 0; y--) {
-                arr[y+1] = arr[y];
-            }
-
-            //* Put temp value at the beginning of the array
-            arr[0] = temp;
-        }
-
-    //* Handle rotations to the left
-    } else {
-
-        for (var x = 0; x < actualMovementsNeeded; x++) {
-            var temp = arr[0];
-
-            //* Loop moves items to the left one index
-            for (var y = 1; y < arr.length; y++) {
+            // Move everything that comes afterwards left one index
+            for (var y = x+1; y < arr.length; y++) {
                 arr[y-1] = arr[y];
-                console.log(arr);
             }
 
-            //* Put temp value at end of array
-            arr[arr.length - 1] = temp;
-            console.log(arr);
+            arr.length--; // Decrease the length of the array by one
+            x--; // To cancel the i++ operation effectively
         }
     }
 }
 
-rotateArr(myArr, 2);
 
+filterRange(myArr, 0, 3);
 console.log(myArr);
+
+//# Concat
+
+
+// var myArr = [1, 2, 4, 6, 7, 8];
